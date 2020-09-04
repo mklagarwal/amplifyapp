fbad9c2ea31b9a1185ca16e99a0876dce9e66b75
Mukul <mukul.agarwal333@gmail.com>
2020-09-04 09:27:50 +0530

    added capability to add image

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://4tokgm7pvndndhjl46hi4lo2sy.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3ivebe4psnamrclhlndv5nmiam"
+    "aws_appsync_apiKey": "da2-3ivebe4psnamrclhlndv5nmiam",
+    "aws_user_files_s3_bucket": "imagebucket225238-dev",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
